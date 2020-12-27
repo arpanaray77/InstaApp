c59d2097efe318daef6519f7557ded8e0be1067e
arpanaray77 <arpanaray75897@gmail.com>
2020-12-28 01:54:38 +0530

    Added Signup Activity

--- app/src/main/java/com/example/instaapp/LoginActivity.kt
+++ app/src/main/java/com/example/instaapp/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.example.instaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signup_link.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
