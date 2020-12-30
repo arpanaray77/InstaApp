f20c77cbfc0274c06f3572574983917ac2c3fde7
arpanaray77 <arpanaray75897@gmail.com>
2020-12-30 12:32:16 +0530

    Adding Login feature using Firebase Authentication

--- app/src/main/java/com/example/instaapp/AccountSettings.kt
+++ app/src/main/java/com/example/instaapp/AccountSettings.kt
@@ -1,11 +1,13 @@
 package com.example.instaapp
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_account_settings.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class AccountSettings : AppCompatActivity() {
@@ -13,5 +15,13 @@ class AccountSettings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
 
+        accountSettings_logoutbtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            //forwarding to home page
+            val intent= Intent(this@AccountSettings,LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
     }
 }
