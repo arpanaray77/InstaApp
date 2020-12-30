6b04b303a3fe2e4d976959316019112c4bfd191d
arpanaray77 <arpanaray75897@gmail.com>
2020-12-30 10:06:52 +0530

    Adding Firebase authentication to SignUp Page

--- app/src/main/java/com/example/instaapp/AccountSettings.kt
+++ app/src/main/java/com/example/instaapp/AccountSettings.kt
@@ -1,11 +1,17 @@
 package com.example.instaapp
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class AccountSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
+
     }
 }
--- app/src/main/java/com/example/instaapp/LoginActivity.kt
+++ app/src/main/java/com/example/instaapp/LoginActivity.kt
@@ -3,6 +3,9 @@ package com.example.instaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +15,19 @@ class LoginActivity : AppCompatActivity() {
 
         signup_link.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(FirebaseAuth.getInstance().currentUser!=null)
+        {
+            //forwarding to home page
+            val intent=Intent(this@LoginActivity,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
         }
     }
 }
