a6efea5aec51c61b87c2b6c7c4263aea3e2ad3ff
arpanaray77 <arpanaray75897@gmail.com>
2021-01-01 02:09:08 +0530

    Adding Picasso Dependency

--- app/src/main/java/com/example/instaapp/Adapter/UserAdapter.kt
+++ app/src/main/java/com/example/instaapp/Adapter/UserAdapter.kt
@@ -1,7 +1,7 @@
 package com.example.instaapp.Adapter
 
 import android.content.Context
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
@@ -9,7 +9,9 @@ import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instaapp.Model.User
 import com.example.instaapp.R
+import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import android.view.View as AndroidViewView
 
 //import com.google.firebase.database.core.view.View
 
@@ -17,18 +19,26 @@ class UserAdapter (private var mContext:Context,
                    private var mUser:List<User>,
                    private var isFragment:Boolean=false):RecyclerView.Adapter<UserAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        //to make user item available in search item
+      val view=LayoutInflater.from(mContext).inflate(R.layout.user_item_layout,parent,false)
+         return UserAdapter.ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mUser.size
     }
 
     override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //to display the user data
+        val user=mUser[position]
+
+        holder.userNameTextView.text=user.getUsername()
+        holder.userFullnameTextView.text=user.getFullname()
+        Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(holder.userProfileImage) //add picasso dependency for image caching and downloading
+
     }
 
-    class ViewHolder(@NonNull itemView: View): RecyclerView.ViewHolder(itemView)
+    class ViewHolder(@NonNull itemView: AndroidViewView): RecyclerView.ViewHolder(itemView)
     {
         var userNameTextView:TextView=itemView.findViewById(R.id.user_item_search_username)
         var userFullnameTextView:TextView=itemView.findViewById(R.id.user_item_search_fullname)
