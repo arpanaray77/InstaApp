9c43a9d3c03dcaa0122930d730f72e4ba330a2b5
arpanaray77 <arpanaray75897@gmail.com>
2021-01-01 02:59:47 +0530

    Working on Search Fragment functioning

--- app/src/main/java/com/example/instaapp/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/instaapp/Fragments/SearchFragment.kt
@@ -1,14 +1,22 @@
 package com.example.instaapp.Fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instaapp.Adapter.UserAdapter
 import com.example.instaapp.Model.User
 import com.example.instaapp.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
 
@@ -19,9 +27,74 @@ class SearchFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_search, container, false)
-        recyclerView= view.findViewById(R.id.recyclerview_search)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        recyclerView = view.findViewById(R.id.recyclerview_search)
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
 
+        mUser = ArrayList()
+        //to show a user on search
+        userAdapter = context?.let {
+            UserAdapter(it, mUser as ArrayList<User>, true)}
+            recyclerView?.adapter = userAdapter
+            view.searchitem.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    if (view.searchitem.text.toString() == "") {
+
+                    }
+                    else
+                    {
+                       recyclerView?.visibility=View.VISIBLE
+                        retrieveUser()
+                        searchUser(s.toString().toLowerCase())
+                    }
+                }
+
+            })
         return view
     }
+
+    private fun searchUser(input:String) {
+
+        val query=FirebaseDatabase.getInstance().getReference()
+            .child("Users")
+            .orderByChild("fullname")
+            .startAt(input)
+            .endAt(input)
+    }
+
+    private fun retrieveUser()
+    {
+        val usersSearchRef=FirebaseDatabase.getInstance().getReference().child("Users")//table name:Users
+        usersSearchRef.addValueEventListener(object:ValueEventListener
+        {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (view?.searchitem?.text.toString() == "") {
+                   mUser?.clear()
+                    for(snapshot in datasnapshot.children)
+                    {
+                        //searching all users
+                        val user=snapshot.getValue(User::class.java))
+                        if(user!=null)
+                        {
+                            mUser?.add(user)
+                        }
+                    }
+                    userAdapter?.notifyDataSetChanged()
+                }
+            }
+        })
+   }
 }
