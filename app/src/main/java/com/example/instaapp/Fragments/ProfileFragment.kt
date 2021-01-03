39e8ba24731b429f66b32affdb4710c48d87f421
arpanaray77 <arpanaray75897@gmail.com>
2021-01-03 23:50:29 +0530

    Working on view User's profile feature

--- app/src/main/java/com/example/instaapp/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/instaapp/Fragments/ProfileFragment.kt
@@ -1,24 +1,21 @@
 package com.example.instaapp.Fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.security.identity.AccessControlProfileId
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instaapp.AccountSettings
 import com.example.instaapp.R
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
+class ProfileFragment: Fragment() {
+    private lateinit var profileId:String
+    private lateinit var firebaseUser:FirebaseUser
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +23,12 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val pref=context?.getSharedPreferences("PREFS", Context.MODE_PRIVATE)
+        if(pref!=null)
+        {
+            this.profileId= pref.getString("profileId","none")!!
+        }
         //to call account profile setting activity
         view.edit_profile_Button.setOnClickListener{
             startActivity(Intent(context,AccountSettings::class.java))
--- app/src/main/java/com/example/instaapp/MainActivity.kt
+++ app/src/main/java/com/example/instaapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.instaapp
 
-import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
