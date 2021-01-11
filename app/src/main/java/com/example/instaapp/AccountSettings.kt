cfb30c3e8ac2927f60ad287251811ab7b459a761
arpanaray77 <arpanaray75897@gmail.com>
2021-01-12 00:17:24 +0530

    Working on Account Settings feature

--- app/src/main/java/com/example/instaapp/AccountSettings.kt
+++ app/src/main/java/com/example/instaapp/AccountSettings.kt
@@ -12,10 +12,7 @@ import android.widget.Toast
 import com.example.instaapp.Model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_account_settings.*
 import kotlinx.android.synthetic.main.activity_account_settings.view.*
@@ -25,6 +22,7 @@ import kotlinx.android.synthetic.main.fragment_profile.view.*
 class AccountSettings : AppCompatActivity() {
 
     private lateinit var firebaseUser: FirebaseUser
+    private var checker= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +34,36 @@ class AccountSettings : AppCompatActivity() {
         accountSettings_logoutbtn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             //forwarding to home page
-            val intent= Intent(this@AccountSettings,LoginActivity::class.java)
+            val intent = Intent(this@AccountSettings, LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
         }
+   //editing user information
+        save_edited_info.setOnClickListener {
+            if (checker == "clicked")
+            {
+
+            }
+            else
+            {
+                 updateUserInfoOnly()
+            }
+
+        }
     }
+
+    private fun updateUserInfoOnly() {
+
+        val userRef : DatabaseReference =FirebaseDatabase.getInstance().reference.child("Users")
+        //using hashmap to store values
+        val userMap=HashMap<String,Any>()
+        userMap["fullname"]=accountSettings_fullname_profile.text.toString().toLowerCase()
+        userMap["username"]=accountSettings_username_profile.text.toString().toLowerCase()
+        userMap["bio"]=accountSettings_bio_profile.text.toString().toLowerCase()
+
+    }
+
     private fun getUserInfo() {
         val usersRef = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser.uid)
         usersRef.addValueEventListener(object : ValueEventListener {
