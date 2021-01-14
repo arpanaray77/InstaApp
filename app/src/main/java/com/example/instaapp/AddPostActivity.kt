bdfa203b0466f6f6bcbc5b4448c69a1f0b731a92
arpanaray77 <arpanaray75897@gmail.com>
2021-01-15 00:36:57 +0530

    working on "Add Post" feature

--- app/src/main/java/com/example/instaapp/AddPostActivity.kt
+++ app/src/main/java/com/example/instaapp/AddPostActivity.kt
@@ -1,40 +1,48 @@
 package com.example.instaapp
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_account_settings.*
 import kotlinx.android.synthetic.main.activity_add_post.*
 
 class AddPostActivity : AppCompatActivity() {
 
-    private  var myUrl=""
-    private  var imageUri: Uri?=null
-    private  var storagePostPictureRef: StorageReference?=null
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+    private var storagePostPictureRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
 
-        storagePostPictureRef= FirebaseStorage.getInstance().reference.child("Post Picture")
+        storagePostPictureRef = FirebaseStorage.getInstance().reference.child("Post Picture")
 
         post_picture.setOnClickListener {
             uploadImage()
         }
         CropImage.activity()
-            .setAspectRatio(1,1)
+            .setAspectRatio(1, 1)
             .start(this@AddPostActivity)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(resultCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode== Activity.RESULT_OK && data!=null) {
+        if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
             val result = CropImage.getActivityResult(data)
             imageUri = result.uri
             profile_image_profile.setImageURI(imageUri)
@@ -43,6 +51,40 @@ class AddPostActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        TODO("Not yet implemented")
+        when {
+            imageUri == null -> Toast.makeText(
+                this,
+                "Please select image first.",
+                Toast.LENGTH_LONG
+            ).show()
+            TextUtils.isEmpty(write_post.text.toString()) -> Toast.makeText(
+                this,
+                "Please write caption",
+                Toast.LENGTH_LONG
+            ).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Posting")
+                progressDialog.setMessage("Please wait, we are posting..")
+                progressDialog.show()
+
+                val fileRef =
+                    storagePostPictureRef!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+                var uploadTask: StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+
+                uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                            progressDialog.dismiss()
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                })
+            }
+        }
     }
 }
