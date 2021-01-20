6cca747b917e29dcb1912b3bfac51e5ab438bc0a
arpanaray77 <arpanaray75897@gmail.com>
2021-01-20 16:44:00 +0530

    Successfully added Post Detail Fragment

--- app/src/main/java/com/example/instaapp/Adapter/MyPostAdapter.kt
+++ app/src/main/java/com/example/instaapp/Adapter/MyPostAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.instaapp.Adapter
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.provider.Settings.Global.putString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.NonNull
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.instaapp.Fragments.PostDetailFragment
+import com.example.instaapp.Fragments.ProfileFragment
+import com.example.instaapp.MainActivity
 import com.example.instaapp.Model.Post
 import com.example.instaapp.R
 import com.squareup.picasso.Picasso
@@ -36,5 +43,14 @@ class MyPostAdapter(private val mContext: Context, private  val mPost:List<Post>
         val post=mPost[position]
 
         Picasso.get().load(post.getPostImage()).into(holder.postedImg)
+        holder.postedImg.setOnClickListener {
+
+                val pref=mContext.getSharedPreferences("PREFS",Context.MODE_PRIVATE).edit()
+                pref.putString("postid",post.getPostId())
+                pref.apply()
+
+                (mContext as FragmentActivity).supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, PostDetailFragment()).commit()
+        }
     }
 }
