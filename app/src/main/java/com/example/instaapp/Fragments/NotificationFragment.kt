d018893e6495ba0822c7bb084b6fff783270021d
arpanaray77 <arpanaray75897@gmail.com>
2021-01-21 00:07:06 +0530

    Working on Notification feature

--- app/src/main/java/com/example/instaapp/Fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/instaapp/Fragments/NotificationFragment.kt
@@ -5,56 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instaapp.Adapter.NotificationAdapter
+import com.example.instaapp.Model.Notification
+import com.example.instaapp.Model.Post
 import com.example.instaapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotificationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var notificationAdapter: NotificationAdapter?=null
+    private var notificationList:MutableList<Notification>?=null
+    private  var firebaseUser: FirebaseUser?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        val view= inflater.inflate(R.layout.fragment_notification, container, false)
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser!!
+        var recyclerView: RecyclerView?=null
+        recyclerView=view.findViewById(R.id.recyclerview_notification)
+        recyclerView.setHasFixedSize(true)
+        val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager=linearLayoutManager
+
+        notificationList=ArrayList()
+        notificationAdapter=context?.let { NotificationAdapter(it,notificationList as ArrayList<Notification>) }
+        recyclerView.adapter=notificationAdapter
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun readNotification() {
+
+        val postRef= FirebaseDatabase.getInstance().reference.child("Notification").child(firebaseUser!!.uid)
+        postRef.addValueEventListener(object : ValueEventListener
+        {
+            override fun onCancelled(error: DatabaseError) {
+
             }
+
+            override fun onDataChange(p0: DataSnapshot)
+            {
+                notificationList?.clear()
+                for (snapshot in p0.children)
+                {
+                    val notification: Notification? = snapshot.getValue(Notification::class.java)
+                    notificationList!!.add(notification!!)
+                }
+                    Collections.reverse(notificationList)
+                    notificationAdapter!!.notifyDataSetChanged()
+
+            }
+        })
     }
 }
